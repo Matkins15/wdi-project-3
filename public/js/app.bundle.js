0df421590045f2d820c20310bea7a5c3ebcc940e
ejoysutton <joy.ericka.sutton@gmail.com>
2017-04-10 23:33:48 -0400

    populated jobs.show.controller and services

--- public/js/app.bundle.js
+++ public/js/app.bundle.js
@@ -375,6 +375,7 @@ function UsersService($http) {
 	self.loadCurrent = loadCurrent;
 	self.addUser = addUser;
 	self.addJob = addJob;
+	self.loadCurrentJob = loadCurrentJob;
 
 	// Load current user
 	function loadCurrent(id) {
@@ -391,6 +392,10 @@ function UsersService($http) {
 		//NEED TO FIND OUT ABOUT GETTING USERID FOR POST
 		console.log('we are in services');
 		return $http.post('/users/' + id + '/jobs');
+	}
+
+	function loadCurrentJob(id) {
+		return $http.get('/users/' + userId + '/jobs/' + id);
 	}
 }
 
