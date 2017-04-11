5a5d82bde40922c46fcaba9739ea8cf5381da367
ejoysutton <joy.ericka.sutton@gmail.com>
2017-04-11 00:19:07 -0400

    Working on notes post BE path, need to find out why is sending to jobs instead of notes

--- client/components/jobs.show/jobs.show.controller.js
+++ client/components/jobs.show/jobs.show.controller.js
@@ -1,7 +1,24 @@
-JobsShowController.$inject = [];
+UserController.$inject = ['$stateParams', 'UsersService'];
 
-function JobsShowController() {
+function JobsShowController($stateParams, UsersService) {
 	const vm = this;
+
+	vm.currentJob = {};
+
+	activate();
+	function activate() {}
+
+	function loadCurrentJob() {
+		console.log($stateParams);
+//make sure when the route loads you make a reference to .userId in the new controller?
+		UsersService
+			.loadCurrentJob($stateParams.id)
+			.then(function resolve(response) {
+				vm.currentJob = response.data.job;
+			});
+	}
+
+
 }
 
 module.exports = JobsShowController;
